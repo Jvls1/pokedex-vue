8d900228a8594a4a43f1aeba474d3919c1f8b9b0
João Vitor <joaovlopes000@gmail.com>
2022-11-16 22:14:19 -0300

    search field start implementation

--- src/components/PokemonList/SearchField.vue
+++ src/components/PokemonList/SearchField.vue
@@ -1,12 +1,33 @@
 <template>
     <div>
         <input type="text" name="search" id="search" placeholder="Search"
-            class="w-full">
+            class="w-full" v-model="pokemonNameOrId" onsubmit(search())/>
     </div>
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            pokemonNameOrId: ''
+        }
+    },
+    methods: {
+        search() {
+            fetch('https://pokeapi.co/api/v2/pokemon/'+this.pokemonNameOrId, {
+                method: "GET"
+            }).then(res => {
+                res.json().then(data => ({
+                data: data,
+                status: res.status
+            })).then(res => {
+                this.pokemon = res.data;
+                let type = res.data.types[0].type.name;
+                this.typeColorCss = this.getRgbColor(type);
+                this.linkImage = res.data.sprites.other["official-artwork"].front_default;
+            });
+            });
+        }
+    },
 }
 </script>
 <style scoped>
